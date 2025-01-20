db0a74d025552b2acad667ae9d731bb800b9ed39
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-01-20 16:09:30 +0530

    Update chainOfResponsibility.cpp

--- Behavioral_Pattern/chain-of-responsibility/chainOfResponsibility.cpp
+++ Behavioral_Pattern/chain-of-responsibility/chainOfResponsibility.cpp
@@ -4,7 +4,7 @@ Chain Of Responsibility:
    aur receiver (lene wale) ke beech loosely couple karta hai. 
    Is pattern me ek request ko ek se zyada objects ke chain ke through forward kiya jata hai jab tak koi ek object us request ko handle na kar le.
 
-
+gg
 
 
 
