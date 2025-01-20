e989e36e10bfa6482e610e8ef8e30d35fabb23bd
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-01-20 10:28:37 +0530

    Update chainOfResponsibility.cpp

--- Behavioral_Pattern/chain-of-responsibility/chainOfResponsibility.cpp
+++ Behavioral_Pattern/chain-of-responsibility/chainOfResponsibility.cpp
@@ -1,3 +1,29 @@
 Chain Of Responsibility:
 --------------------------
+1. Chain of Responsibility pattern ek aisa design pattern hai jo request ko uske sender (bhejne wale) 
+   aur receiver (lene wale) ke beech loosely couple karta hai. 
+   Is pattern me ek request ko ek se zyada objects ke chain ke through forward kiya jata hai jab tak koi ek object us request ko handle na kar le.
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
