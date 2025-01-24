2f16e3e6720db065f70e6bde30ef2ae4ef7fe94c
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-01-24 14:05:39 +0530

    Update factory_design.cpp

--- CreationalDesignPatterns/FactoryDesignPattern/factory_design.cpp
+++ CreationalDesignPatterns/FactoryDesignPattern/factory_design.cpp
@@ -1,1 +1,14 @@
 
+
+
+
+
+
+
+
+
+
+
+Key Differences with Abstract Factory
+Factory Method: Sirf ek product type ke liye method provide karta hai.
+Abstract Factory: Multiple related products ke liye factory methods provide karta hai.
