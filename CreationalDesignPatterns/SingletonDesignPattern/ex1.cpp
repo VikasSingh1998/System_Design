8bb75779f9dc1a0d23160a20811e5bb3011587e8
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-02-05 13:46:19 +0530

    Update ex1.cpp

--- CreationalDesignPatterns/SingletonDesignPattern/ex1.cpp
+++ CreationalDesignPatterns/SingletonDesignPattern/ex1.cpp
@@ -1,1 +1,14 @@
+Singleton Design Pattern 🚀
+📌 Definition
+The Singleton Design Pattern ensures that only one instance of a class is created and provides a global point of access to that instance.
 
+🔥 Why Use Singleton?
+Restricts multiple object creation → Saves memory.
+Ensures global access → All parts of the program share the same instance.
+Useful for shared resources → Example: Logger, Database Connection, Configuration Manager.
+
+
+📌 Key Features
+Private Constructor → Prevents external instantiation.
+Static Instance Variable → Stores the single instance.
+Static getInstance() Method → Controls access to the instance.
