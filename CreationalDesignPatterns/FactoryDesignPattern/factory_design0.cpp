eb85e0e377e08fcf7f6da086e96282792718aff8
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-02-05 12:51:18 +0530

    Update factory_design0.cpp

--- CreationalDesignPatterns/FactoryDesignPattern/factory_design0.cpp
+++ CreationalDesignPatterns/FactoryDesignPattern/factory_design0.cpp
@@ -1,1 +1,77 @@
+#include <iostream>
+using namespace std;
+
+// Abstract class representing a vehicle
+class Vehicle 
+{
+public:
+    virtual void printVehicle() = 0; // Pure virtual function
+    virtual ~Vehicle() {}           // Virtual destructor
+};
+
+// Concrete class for Two-Wheeler
+class TwoWheeler : public Vehicle 
+{
+public:
+    void printVehicle() override 
+    {
+        cout << "I am two wheeler" << endl;
+    }
+};
+
+// Concrete class for Four-Wheeler
+class FourWheeler : public Vehicle 
+{
+public:
+    void printVehicle() override {
+        cout << "I am four wheeler" << endl;
+    }
+};
+
+// Factory Interface
+class VehicleFactory 
+{
+public:
+    virtual Vehicle* createVehicle() = 0; // Factory method
+    virtual ~VehicleFactory() {}
+};
+
+// Concrete Factory for TwoWheeler
+class TwoWheelerFactory : public VehicleFactory 
+{
+public:
+    Vehicle* createVehicle() override 
+    {
+        return new TwoWheeler();
+    }
+};
+
+// Concrete Factory for FourWheeler
+class FourWheelerFactory : public VehicleFactory 
+{
+public:
+    Vehicle* createVehicle() override 
+    {
+        return new FourWheeler();
+    }
+};
+
+int main() 
+{
+    VehicleFactory* twoWheelerFactory = new TwoWheelerFactory();
+    Vehicle* twoWheeler = twoWheelerFactory->createVehicle();
+    twoWheeler->printVehicle();
+
+    VehicleFactory* fourWheelerFactory = new FourWheelerFactory();
+    Vehicle* fourWheeler = fourWheelerFactory->createVehicle();
+    fourWheeler->printVehicle();
+
+    // Clean up memory
+    delete twoWheeler;
+    delete twoWheelerFactory;
+    delete fourWheeler;
+    delete fourWheelerFactory;
+
+    return 0;
+}
 
